2c3c8e0209710d93d4820db58580a079322b58be
Akira Sawada <aklaswad@gmail.com>
2010-11-19 23:03:03 +0900

    perltidy and a bit cleanup.

--- plugins/RegistryViewer/lib/RegistryViewer.pm
+++ plugins/RegistryViewer/lib/RegistryViewer.pm
@@ -26,34 +26,38 @@ sub uri {
 }
 
 sub view {
-    my $app = shift;
-    my $path = $app->param('path');
+    my $app    = shift;
+    my $path   = $app->param('path');
     my $mt_nav = $app->param('mt_nav');
     $mt_nav = 1 if !defined $mt_nav;
     local $app->{__mt_nav} = $mt_nav;
-    my @paths = split ',', $path;
+    my @paths  = split ',', $path;
     my $plugin = MT->component('RegistryViewer');
-    my $param = run($plugin, @paths)
+    my $param  = run( $plugin, @paths )
         or return $app->error( $plugin->errstr );
-    $param->{mt_nav} = $mt_nav;
-    $param->{toggle_mt_nav_link} = uri({
-        path   => \@paths,
-        mt_nav => $mt_nav ? 0 : 1,
-    });
+    $param->{mt_nav}             = $mt_nav;
+    $param->{toggle_mt_nav_link} = uri(
+        {   path   => \@paths,
+            mt_nav => $mt_nav ? 0 : 1,
+        }
+    );
+
     if ( !$mt_nav ) {
-        $param->{build_menus} = 0;
+        $param->{build_menus}         = 0;
         $param->{build_blog_selector} = 0;
         $param->{build_compose_menus} = 0;
     }
-    return $app->load_tmpl('registry_viewer.tmpl', $param);
+    return $app->load_tmpl( 'registry_viewer.tmpl', $param );
 }
 
 sub run {
     my $plugin = shift;
-    my @paths = @_;
+    my @paths  = @_;
     my %param;
     my $current_path = $paths[-1];
     my @parent_path;
+
+    ## Make breadcrumb
     my @breadcrumb;
     $param{root_link} = my $root_link = uri();
     push @breadcrumb, { link => $root_link, label => '(root)' };
@@ -64,26 +68,34 @@ sub run {
     }
     $param{breadcrumb} = \@breadcrumb;
 
+    ## Get description of current path.
     my @descriptions = find_desc( path => \@paths );
     $param{descriptions} = \@descriptions;
 
+    ## Get Possible values.
+    my @wild_descs = find_desc( path => [ @paths, '+' ] );
+    $param{wild_descs} = [
+        sort { $a->{name} cmp $b->{name} }
+        grep { $_->{for} =~ m/\*|\(\w+\)/ } @wild_descs
+        ]
+        if scalar @wild_descs;
+
+    ## Get child values from real registry.
     my @components = sort keys %MT::Components;
     my $last_type;
     my %registries;
-    for my $c ( @components ) {
+    for my $c (@components) {
         my $reg;
-        eval {
-            $reg = MT->component($c)->registry(@paths);
-        };
-        if ( $@ ) {
+        eval { $reg = MT->component($c)->registry(@paths); };
+        if ($@) {
             my @parent_path = @paths;
-            my $me = pop @parent_path;
+            my $me          = pop @parent_path;
             $reg = MT->component($c)->registry(@parent_path);
             $reg = $reg->{$me} if $me;
         }
         next unless defined $reg;
         my $type = ref $reg || 'scalar';
-        if ( $last_type ) {
+        if ($last_type) {
             $type eq $last_type
                 or return $plugin->errtrans('Multiple types mixed registry.');
         }
@@ -91,9 +103,6 @@ sub run {
         $registries{$c} = $reg;
     }
     my $type = $param{type} = $last_type;
-    my @wild_descs = find_desc( path => [ @paths, '+' ] );
-    $param{wild_descs} = [ sort { $a->{name} cmp $b->{name} } grep { $_->{for} =~ m/\*|\(\w+\)/ } @wild_descs ]
-        if scalar @wild_descs;
 
     if ( $type eq 'HASH' ) {
         my %wild_desc = map { $_->{name} => $_ } @wild_descs;
@@ -109,20 +118,22 @@ sub run {
                 my $c_value = $hash->{$child};
                 next if !defined $c_value;
                 $c_value = ref $c_value if ref $c_value;
-                push @values, {
-                    component => $c,
-                    value     => $c_value,
-                };
+                push(
+                    @values,
+                    {   component => $c,
+                        value     => $c_value,
+                    }
+                );
             }
             my @descs = find_desc( path => \@child_paths, wild => 0 );
-            push @child_keys,
-
-            {
-            link   => $link,
-            name   => $child,
-            values => \@values,
-            descs  => \@descs
-            };
+            push(
+                @child_keys,
+                {   link   => $link,
+                    name   => $child,
+                    values => \@values,
+                    descs  => \@descs
+                }
+            );
         }
         $param{child_keys} = \@child_keys;
     }
@@ -132,7 +143,8 @@ sub run {
             my $reg = $registries{$c};
             my %reg_param;
             if ( $current_path =~ /label$/ ) {
-                $reg_param{registry_value} = ref $reg eq 'CODE' ? $reg->() : $reg;
+                $reg_param{registry_value}
+                    = ref $reg eq 'CODE' ? $reg->() : $reg;
             }
             elsif ( 'ARRAY' eq ref $reg ) {
                 $reg_param{registry_values} = $reg;
@@ -145,7 +157,7 @@ sub run {
             }
             else {
                 require MT::Util::YAML;
-                my $dump =  MT::Util::YAML::Dump $reg;
+                my $dump = MT::Util::YAML::Dump $reg;
                 $reg_param{registry_value} = qq{<pre>$dump</pre>};
             }
             $reg_param{__component} = $c;
@@ -157,34 +169,41 @@ sub run {
 }
 
 sub find_desc {
-    my ( %opts ) = @_;
-    my $path         = $opts{path};
-    my $depth        = $opts{depth} || 0;
-    my $wild         = exists $opts{wild} ? $opts{wild} : 1;
-    my $orig_path    = exists $opts{orig_path} ? $opts{orig_path} : [ @$path ];
+    my (%opts) = @_;
+    my $path = $opts{path};
+    my $base_path = $opts{base_path} || 'registry_descriptions';
+    my $depth     = $opts{depth}     || 0;
+    my $wild      = exists $opts{wild}      ? $opts{wild}      : 1;
+    my $orig_path = exists $opts{orig_path} ? $opts{orig_path} : [@$path];
     my $current_path = $opts{current_path} || [];
     return if !defined $path;
 
     my @rest_path = @$path;
-    my $r = MT->registry('registry_descriptions', @$current_path);
+    my $r = MT->registry( $base_path, @$current_path );
     if ( 'HASH' ne ref $r ) {
-        return ( make_desc(
-            name      => $opts{name},
-            value     => $r,
-            path      => $current_path,
-            orig_path => $orig_path,
-        )) if !scalar @rest_path;
+        return (
+            make_desc(
+                name      => $opts{name},
+                value     => $r,
+                path      => $current_path,
+                orig_path => $orig_path,
+            )
+        ) if !scalar @rest_path;
         return ();
     }
 
     my @results;
     if ( $r->{_} && !scalar @rest_path ) {
-        push @results, ( make_desc(
-            name      => $opts{name},
-            value     => $r->{_},
-            path      => $current_path,
-            orig_path => $orig_path,
-        ));
+        push(
+            @results,
+            (   make_desc(
+                    name      => $opts{name},
+                    value     => $r->{_},
+                    path      => $current_path,
+                    orig_path => $orig_path,
+                )
+            )
+        );
     }
 
     my @this_key;
@@ -208,14 +227,18 @@ sub find_desc {
                 local $" = '/';
                 $match = $hash_key =~ /^@this_key$/;
             }
-            if ( $match ) {
-                push @results, find_desc(
-                    path         => \@rest_path,
-                    depth        => $depth + 1,
-                    wild         => $wild,
-                    orig_path    => $orig_path,
-                    name         => $hash_key,
-                    current_path => [ @$current_path, $hash_key ],
+            if ($match) {
+                push(
+                    @results,
+                    find_desc(
+                        path         => \@rest_path,
+                        base_path    => $base_path,
+                        depth        => $depth + 1,
+                        wild         => $wild,
+                        orig_path    => $orig_path,
+                        name         => $hash_key,
+                        current_path => [ @$current_path, $hash_key ],
+                    )
                 );
             }
         }
@@ -224,9 +247,9 @@ sub find_desc {
 }
 
 sub make_desc {
-    my ( %opts ) = @_;
+    my (%opts)    = @_;
     my $values    = $opts{value};
-    my @path      = @{$opts{path}};
+    my @path      = @{ $opts{path} };
     my $name      = $opts{name};
     my $orig_path = $opts{orig_path};
     $values = [$values] unless ref $values;
@@ -237,17 +260,19 @@ sub make_desc {
         $name = pop @names;
     }
     my @descs;
-    for my $desc ( @$values ) {
+    for my $desc (@$values) {
         if ( $desc =~ /\s*sub\s*\{/ || $desc =~ /^\$.*::/ ) {
             my $code = MT->handler_to_coderef($desc);
-            $desc = $code->($orig_path, \@path);
+            $desc = $code->( $orig_path, \@path );
         }
-        push @descs, {
-            name => $name,
-            for  => join( '/', @path),
-            desc => $desc,
-            link => uri( @path ),
-        };
+        push(
+            @descs,
+            {   name => $name,
+                for  => join( '/', @path ),
+                desc => $desc,
+                link => uri(@path),
+            }
+        );
     }
     return @descs;
 }
